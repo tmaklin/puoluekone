b5500b6c23a57889f2d9a91a6e447fe07acdffea
Mäklin <tommi.maklin@helsinki.fi>
2019-03-24 13:16:39 +0200

    add the GPLv3 license notice to relevant files

--- read_and_write.R
+++ read_and_write.R
@@ -1,3 +1,18 @@
+## This file is part of Puoluekone.
+##
+## Puoluekone is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Puoluekone is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Puoluekone.  If not, see <https://www.gnu.org/licenses/>.
+
 WriteToDisk <- function(parties, out.ecs, out.weights, out.info) {
     ## Create a hash to use as the folder name if writing to disk
     out.folder <- sprintf("%s_%s", as.integer(Sys.time()), digest::digest(data))
--- read_bitfield.cpp
+++ read_bitfield.cpp
@@ -1,3 +1,19 @@
+// This file is a modified version of the one included in mSWEEP
+// <https://github.com/PROBIC/mSWEEP> and a part of Puoluekone.
+//
+// Puoluekone is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Puoluekone is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Puoluekone.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <string>
 #include <fstream>
 #include <sstream>
