8c64f144258df4deec6b40fe66c55489ef717e05
Tommi Mäklin <tommi@maklin.fi>
2020-02-06 13:50:09 +0200

    update license

--- read_bitfield.cpp
+++ read_bitfield.cpp
@@ -1,18 +1,19 @@
 // This file is a modified version of the one included in mSWEEP
 // <https://github.com/PROBIC/mSWEEP> and a part of Puoluekone.
+// Copyright (C) 2020  Tommi Mäklin (tommi@maklin.fi)
 //
-// Puoluekone is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Puoluekone is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Affero General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with Puoluekone.  If not, see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <string>
 #include <fstream>
